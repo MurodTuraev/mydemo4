fdff15638f497c983fcfe9a6e75d7597ab428962
macshop <tml.murod@mail.ru>
2023-04-08 13:20:30 +0500

    pushRaplacementNamed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mydemo4/buttonOne.dart';
+import 'package:mydemo4/firstPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +26,11 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DetailPage(),
+      routes: {
+        DetailPage.id: (context) => DetailPage(),
+        FirstPage.id: (context) => FirstPage(),
+      },
     );
   }
 }
